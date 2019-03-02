aedd8c2c5ff1ef94ba3dd05aebe00af0ae7a6a03
alexmaehon <alexmaehon@163.com>
2019-03-02 11:10:17 +0800

    ss-libev and encryption method

---
+++ ss-libev_install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+git clone https://github.com/shadowsocks/shadowsocks-libev.git
+cd shadowsocks-libev
+git submodule update --init --recursive
+
+# Installation of basic build dependencies
+apt-get install --no-install-recommends gettext build-essential autoconf libtool libpcre3-dev asciidoc xmlto libev-dev libc-ares-dev automake libmbedtls-dev libsodium-dev
+
+# Installation of libsodium
+export LIBSODIUM_VER=1.0.16
+wget https://download.libsodium.org/libsodium/releases/libsodium-$LIBSODIUM_VER.tar.gz
+tar xvf libsodium-$LIBSODIUM_VER.tar.gz
+pushd libsodium-$LIBSODIUM_VER
+./configure --prefix=/usr && make
+make install
+popd
+ldconfig
+
+# Installation of MbedTLS
+export MBEDTLS_VER=2.6.0
+wget https://tls.mbed.org/download/mbedtls-$MBEDTLS_VER-gpl.tgz
+tar xvf mbedtls-$MBEDTLS_VER-gpl.tgz
+pushd mbedtls-$MBEDTLS_VER
+make SHARED=1 CFLAGS=-fPIC
+make DESTDIR=/usr install
+popd
+ldconfig
+
+apt update
+apt upgrade -y
+apt install shadowsocks-libev -y
+ufw allow 407
+ufw allow 407/udp
+
+cd ~
+nano /etc/shadowsocks-libev/config.json

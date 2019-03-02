02eeabbe843db9ab2384d5089c348eff5fe92e09
alexmaehon <alexmaehon@163.com>
2019-03-02 12:04:41 +0800

    Add files via upload

--- test1.sh
+++ test1.sh
@@ -5,7 +5,9 @@ git clone https://github.com/shadowsocks/shadowsocks-libev.git
 cd shadowsocks-libev
 git submodule update --init --recursive
 
+apt-get update
 mkdir addtion_libary
-cd addtion_libary
+pushd addtion_libary
 # Installation of basic build dependencies
 apt-get -y install --no-install-recommends gettext build-essential autoconf libtool libpcre3-dev asciidoc xmlto libev-dev libc-ares-dev automake libmbedtls-dev libsodium-dev
+popd

b4ad6c5748fd107905e82daf05b8ed5bf915c954
alexmaehon <alexmaehon@163.com>
2019-03-02 13:42:10 +0800

    Add files via upload

---
+++ teddysun_ss_libev_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-libev-debian.sh
+
+chmod +x shadowsocks-libev-debian.sh
+
+./shadowsocks-libev-debian.sh 2>&1 | tee shadowsocks-libev-debian.log
